675f363e81264c35a50cd8bcba050228e08bcb8c
amberling1988 <Selvin578299153@yeah.net>
2015-12-30 17:10:25 +0800

    Initial Commit

---
+++ getScreenTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  getScreenTest
+//
+//  Created by 123 on 15/12/30.
+//  Copyright (c) 2015年 lb. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ getScreenTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  getScreenTest
+//
+//  Created by 123 on 15/12/30.
+//  Copyright (c) 2015年 lb. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
